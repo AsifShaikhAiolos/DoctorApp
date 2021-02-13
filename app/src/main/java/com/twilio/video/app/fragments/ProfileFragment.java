99e61b7e82e2eba3d3b36e8a9b2f7e002851a37a
asifshaikh <asif@example.com>
2021-02-13 20:09:35 +0000

    complete design of patient

--- app/src/main/java/com/twilio/video/app/fragments/ProfileFragment.java
+++ app/src/main/java/com/twilio/video/app/fragments/ProfileFragment.java
@@ -1,13 +1,16 @@
 package com.twilio.video.app.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 
+import com.twilio.video.app.Edit_View_ProfileActivity;
 import com.twilio.video.app.R;
 
 public class ProfileFragment extends Fragment {
@@ -21,6 +24,16 @@ public class ProfileFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
         getActivity().setTitle("Profile");
+
+        ConstraintLayout editProfile=view.findViewById(R.id.editProfile);
+
+        editProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getActivity(), Edit_View_ProfileActivity.class);
+                startActivity(intent);
+            }
+        });
         return view;
     }
 }
